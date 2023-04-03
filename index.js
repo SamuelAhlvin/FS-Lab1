30ddc2b142e04f1ada9855737db7978cf03b7e8b
SamuelAhlvin <ahlvinsamme@gmail.com>
2023-04-03 15:03:30 +0200

    index.html

--- index.js
+++ index.js
@@ -16,4 +16,8 @@ async function main() {
 
 app.listen(process.env.port, () => {
   console.log("Server listening on port: " + process.env.port);
-}) 
+})
+
+app.get('/', (req, res) => {
+  res.sendFile('/index.html', { root: __dirname })
+})
