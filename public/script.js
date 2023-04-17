43b41cd5028cddfeba55dea65db6917d9504f05a
SamuelAhlvin <ahlvinsamme@gmail.com>
2023-04-17 15:47:59 +0200

    Update and delete buttons

--- public/script.js
+++ public/script.js
@@ -6,16 +6,38 @@ fetch('http://localhost:3000/api/albums')
       const albumRow = document.createElement('tr');
       const albumTitle = document.createElement('td');
       albumTitle.textContent = album.title;
+
       const artist = document.createElement('td');
       artist.textContent = album.artist;
+
       const year = document.createElement('td');
       year.textContent = album.year;
+
       const id = document.createElement('td');
       id.textContent = album._id;
+
+      const updateElement = document.createElement('td');
+      const updateButton = document.createElement('button');
+      updateButton.textContent = 'Update';
+      updateButton.addEventListener('click', () => {
+        console.log("update clicked");
+      });
+      updateElement.appendChild(updateButton);
+
+      const deleteElement = document.createElement('td');
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete';
+      deleteButton.addEventListener('click', () => {
+        console.log("delete clicked");
+      });
+      deleteElement.appendChild(deleteButton);
+
       albumRow.appendChild(albumTitle);
       albumRow.appendChild(artist);
       albumRow.appendChild(year);
       albumRow.appendChild(id);
+      albumRow.appendChild(updateElement);
+      albumRow.appendChild(deleteElement);
       albumsTable.appendChild(albumRow);
     });
   })
